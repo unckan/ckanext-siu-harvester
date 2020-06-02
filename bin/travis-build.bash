aee6c8d8e0488ca3495e65dd605a25b802e6be53
Andres Vazquez <andres@data99.com.ar>
2020-06-02 01:49:13 -0300

    Install redis for CI

--- bin/travis-build.bash
+++ bin/travis-build.bash
@@ -4,8 +4,9 @@ set -e
 echo "This is travis-build.bash..."
 
 echo "Installing the packages that CKAN requires..."
+sudo add-apt-repository ppa:chris-lea/redis-server
 sudo apt-get update -qq
-sudo apt-get install solr-jetty
+sudo apt-get install solr-jetty redis-server
 
 echo "Installing CKAN and its Python dependencies..."
 git clone https://github.com/ckan/ckan
