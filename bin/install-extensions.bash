5cf437d47118724387f090100c2be693b2adc595
Andres Vazquez <andres@data99.com.ar>
2021-08-05 23:50:32 -0300

    Fix ext 2

--- bin/install-extensions.bash
+++ bin/install-extensions.bash
@@ -3,5 +3,5 @@ set -euo pipefail
 
 pip install setuptools-rust
 
-git clone --depth 1 --branch release-2.1.0 https://github.com/ckan/ckanext-harvest
+git clone --depth 1 https://github.com/ckan/ckanext-harvest
 (cd ckanext-harvest && python3 setup.py develop)
