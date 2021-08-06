9739ddb157e9bf112e69228c0649ddbbf1f68732
Andres Vazquez <andres@data99.com.ar>
2021-08-05 23:58:51 -0300

    reqs?

--- bin/install-extensions.bash
+++ bin/install-extensions.bash
@@ -4,4 +4,4 @@ set -euo pipefail
 pip install setuptools-rust
 
 git clone --depth 1 https://github.com/ckan/ckanext-harvest
-(cd ckanext-harvest && python3 setup.py develop)
+(cd ckanext-harvest && pip install -r requirements.txt && python3 setup.py develop)
